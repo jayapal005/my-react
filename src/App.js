e0b243cbf4705841fb3af9c6598fe036c6a43642
jayapal005 <96974007+jayapal005@users.noreply.github.com>
2022-01-08 21:54:19 -0800

    Add files via upload
    my-react  first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="condainer">
+        <div className="dhanush">
+        <img src="https://res.cloudinary.com/urbanclap/image/upload/images/growth/home-screen/1631097450980-d2de38.png"></img>
+        </div>
+        <h5 id="jj1">Blog</h5>
+        <h5 id="jj2">Register As A Professnol</h5>
+        <h5 id="jj3" >Login/Sing Up</h5>
+        <ul>
+          <h3 id="j2">home / chennai</h3>
+        </ul>
+        <h1 id="j1">Home services, on demand.</h1>
+        <select>
+          <option id="j4">chennai</option>
+        </select>
+        <input id="j3" placeholder="Search for services"></input>
+        <h5>Message for Men, Home painting, Electricians etc</h5>
+     
+       </div>
+
+<div className="main">
+<a href="#"> <img id="n1" src="https://res.cloudinary.com/urbanclap/image/upload/q_auto,f_auto,fl_progressive:steep,w_64/t_high_res_template/images/growth/home-screen/1609757635235-1a139e.png"></img>
+<p id="icon1">Salon for women</p></a>
+
+
+<div className="ic2">
+  <a href="">
+    <img id="n2" src="https://res.cloudinary.com/urbanclap/image/upload/q_auto,f_auto,fl_progressive:steep,w_64/t_high_res_template/images/growth/home-screen/1629973621437-ce5af9.png"></img>
+  </a>
+  <p id="i2">Hair services for <br></br>women</p>
+</div>
+
+<a href="#"> <img id="n3" src="https://res.cloudinary.com/urbanclap/image/upload/q_auto,f_auto,fl_progressive:steep,w_64/t_high_res_template/categories/category_v2/category_1312fb60.png"></img></a>
+<p id="ic3">Spa For Women</p>
+</div>
+
+    </div>
+  );
+}
+
+export default App;
